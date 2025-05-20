f4eed17e740e25a574cdf1b6e2e40d0ed85cc776
utkarshsingh004 <glokendra299@gmail.com>
2025-05-21 04:48:09 +0530

    user login and logout using access &  refresh token

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ import asyncHandler from "../utils/asyncHandler";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.models";
 
-export const verifyJWT = asyncHandler(async(req,res,next) => {
+export const verifyJWT = asyncHandler(async(req,_,next) => {
    try {
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer", "")
 
